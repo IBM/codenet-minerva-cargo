3820f080f4e1c39eda064489ec1cd77de11e6339
Rahul Krishna <i.m.ralk@gmail.com>
2023-07-17 20:16:02 -0400

    Consolidated output
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>

--- cargo/standalone.py
+++ cargo/standalone.py
@@ -19,37 +19,65 @@ import click
 import json
 from cargo import Cargo
 
+
 @click.command()
-@click.option('--max-partitions', '-k', default=-1, type=int, help='The maximum number of partitions')
-@click.option('--app-dependency-graph', '-i', type=click.Path(exists=True), 
-              help='Path to the input JSON file. This is a System Dependency Graph, '
-              'you can use the tool from https://github.com/konveyor/dgi-code-analyzer '
-              'to get the system dependency graph of an application.')
-@click.option('--seed-partitions', '-s', type=click.Path(exists=True), default = None, help='Path to the initial seed partitions JSON file')
-@click.option('--output', '-o', type=click.Path(path_type=Path), default = Path.cwd(), help='Path to save the output JSON file')
-def minerva_cargo(max_partitions, app_dependency_graph, seed_partitions, output):
+@click.option(
+    "--max-partitions",
+    "-k",
+    default=-1,
+    type=int,
+    help="The maximum number of partitions",
+)
+@click.option(
+    "--app-dependency-graph",
+    "-i",
+    type=click.Path(exists=True),
+    help="Path to the input JSON file. This is a System Dependency Graph, "
+    "you can use the tool from https://github.com/konveyor/dgi-code-analyzer "
+    "to get the system dependency graph of an application.",
+)
+@click.option(
+    "--transactions",
+    "-t",
+    type=click.Path(exists=True),
+    default=None,
+    help="Path to the discovered transactions JSON file",
+)
+@click.option(
+    "--seed-partitions",
+    "-s",
+    type=click.Path(exists=True),
+    default=None,
+    help="Path to the initial seed partitions JSON file",
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(path_type=Path),
+    default=Path.cwd(),
+    help="Path to save the output JSON file",
+)
+def minerva_cargo(
+    max_partitions, app_dependency_graph, transactions, seed_partitions, output
+):
     """
-    CLI version of CARGO a un-/semi-supervised partition refinement technique that uses a system dependence 
+    CLI version of CARGO a un-/semi-supervised partition refinement technique that uses a system dependence
     graph built using context and flow-sensitive static analysis of a monolithic application.
     """
 
     if seed_partitions is None:
-        seed_partitions = 'auto'
+        seed_partitions = "auto"
 
-    cargo = Cargo(json_sdg_path=app_dependency_graph)
-    _, method_partitions, class_partitions = cargo.run(
-        init_labels=seed_partitions,
-        max_part=max_partitions)
+    cargo = Cargo(json_sdg_path=app_dependency_graph, transactions_json=transactions)
+    partitions = cargo.run(init_labels=seed_partitions, max_part=max_partitions)
 
-    with open(output.joinpath('method_partitions.json'), 'w') as partitions_file:
-        json.dump(method_partitions, partitions_file, indent=4, sort_keys=False)
-    
-    with open(output.joinpath('class_partitions.json'), 'w') as partitions_file:
-        json.dump(class_partitions, partitions_file, indent=4, sort_keys=False)
+    with open(output.joinpath("partitions.json"), "w") as partitions_file:
+        json.dump(partitions, partitions_file, indent=4, sort_keys=False)
     
 
 def main():
     minerva_cargo()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- cargo/utils/transform_graph.py
+++ cargo/utils/transform_graph.py
@@ -16,71 +16,269 @@
 
 from ast import Dict, List
 from collections import defaultdict, Counter
-from copy import deepcopy
-from statistics import mode
+from statistics import mode, mean, variance
+from math import log2
 from ipdb import set_trace
+import re
+from mo_sql_parsing import parse
 
 
 class TransformGraph:
+    nodes = Dict()
+    links = Dict()
+    max_partitions = -1
+
+    @classmethod
+    def _transactions_to_graph(cls, transactions_as_dict: Dict):
+        """ """
+
+        def __extract_class_and_method_names(input_string):
+            # Regular expression pattern to find the class and method names
+            pattern = "L(.*?), (.*?)[(]"
+            match = re.search(pattern, input_string)
+            if match:
+                # Class and method names found
+                class_name = match.group(1).replace("/", ".")
+                method_name = match.group(2)
+                return class_name, method_name
+            else:
+                # No class or method name found
+                return None, None
+
+        for transaction_entity in transactions_as_dict:
+            if transaction_entity["transactions"]:
+                for transaction in transaction_entity["transactions"]:
+                    for txn in transaction["transaction"]:
+                        if txn["sql"] in ["BEGIN", "COMMIT", "ROLLBACK"]:
+                            continue
+
+                        class_name, method_name = __extract_class_and_method_names(
+                            txn["stacktrace"][-1]["method"]
+                        )
+
+                        sql_str = txn["sql"].replace("?", "PLACEHOLDER")
+                        try:
+                            parsed_sql = parse(sql_str)
+                            if "from" in parsed_sql.keys():
+                                txn_type = "READ"
+                                db_table = parsed_sql["from"]["value"].upper()
+                                if db_table not in cls.nodes:
+                                    cls.nodes[db_table] = {
+                                        "name": db_table,
+                                        "type": "SQLTable",
+                                        "centrality": 0,
+                                        "children": [],
+                                    }
+                                if (class_name, db_table, txn_type) not in cls.links:
+                                    cls.links[(class_name, db_table, txn_type)] = {
+                                        "source": db_table,
+                                        "target": class_name,
+                                        "children": [
+                                            {
+                                                "source": db_table,
+                                                "target": ".".join(
+                                                    [class_name, method_name]
+                                                ),
+                                            }
+                                        ],
+                                        "weight": 1,
+                                        "type": txn_type,
+                                    }
+                                else:
+                                    found = False
+                                    for children in cls.links[
+                                        (class_name, db_table, txn_type)
+                                    ]["children"]:
+                                        if (
+                                            ".".join([class_name, method_name])
+                                            in children["target"]
+                                        ):
+                                            cls.links[(class_name, db_table, txn_type)][
+                                                "weight"
+                                            ] += 1
+                                            found = True
+                                    if not found:
+                                        cls.links[(class_name, db_table, txn_type)][
+                                            "children"
+                                        ].append(
+                                            {
+                                                "source": db_table,
+                                                "target": ".".join(
+                                                    [class_name, method_name]
+                                                ),
+                                            }
+                                        )
+
+                            if "update" in parsed_sql.keys():
+                                txn_type = "WRITE"
+                                db_table = parsed_sql["update"].upper()
+                                if db_table not in cls.nodes:
+                                    cls.nodes[db_table] = {
+                                        "id": db_table,
+                                        "type": "SQLTable",
+                                        "methods": [],
+                                        "centrality": 0,
+                                        "method_partitions": [None],
+                                    }
+                                if (class_name, db_table, txn_type) not in cls.links:
+                                    cls.links[(class_name, db_table, txn_type)] = {
+                                        "source": class_name,
+                                        "target": db_table,
+                                        "children": [
+                                            {
+                                                "target": db_table,
+                                                "source": ".".join(
+                                                    [class_name, method_name]
+                                                ),
+                                            }
+                                        ],
+                                        "weight": 1,
+                                        "type": txn_type,
+                                    }
+                                else:
+                                    found = False
+                                    for children in cls.links[
+                                        (class_name, db_table, txn_type)
+                                    ]["children"]:
+                                        if (
+                                            ".".join([class_name, method_name])
+                                            in children["source"]
+                                        ):
+                                            cls.links[(class_name, db_table, txn_type)][
+                                                "weight"
+                                            ] += 1
+                                            found = True
+                                    if not found:
+                                        cls.links[(class_name, db_table, txn_type)][
+                                            "children"
+                                        ].append(
+                                            {
+                                                "target": db_table,
+                                                "source": ".".join(
+                                                    [class_name, method_name]
+                                                ),
+                                            }
+                                        )
+                        except:
+                            pass
+
     @classmethod
     def _method_node_to_class_node(cls, method_nodes: List) -> List:
         """ """
-        classes = defaultdict()
+
+        cls.nodes = defaultdict()
 
         for method_node in method_nodes:
             _class = method_node["class"]
 
-            if _class not in classes:
+            if _class not in cls.nodes:
                 class_node = {
-                    "id": _class,
+                    "name": _class,
                     "methods": {method_node["method"]},
                     "method_partitions": [method_node["partition"]],
+                    "centrality": [method_node["centrality"]],
+                    "type": "ClassNode",
                 }
+                cls.nodes[_class] = class_node
 
             else:
-                class_node = classes[_class]
+                class_node = cls.nodes[_class]
                 class_node["methods"].add(method_node["method"])
+                class_node["centrality"].append(method_node["centrality"])
                 class_node["method_partitions"].append(method_node["partition"])
 
-            classes[_class] = class_node
-
         # Regroup method partitions to find a class partition assigment.
-        for key, class_node in classes.items():
-            classes[key]["methods"] = list(class_node["methods"])
-            classes[key]["class_partition"] = mode(class_node["method_partitions"])
+        for key, class_node in cls.nodes.items():
+            class_node["children"] = [
+                {
+                    "name": method_name,
+                    "partition": partition,
+                    "centrality": centrality,
+                    "type": "MethodNode",
+                }
+                for method_name, partition, centrality in zip(
+                    class_node["methods"],
+                    class_node["method_partitions"],
+                    class_node["centrality"],
+                )
+            ]
+            cls.nodes[key]["class_partition"] = mode(class_node["method_partitions"])
+            cls.nodes[key]["centrality"] = mean(class_node["centrality"])
+            cls.nodes[key]["uncertainity"] = (
+                0
+                if len(class_node["method_partitions"]) < 2
+                else variance(class_node["method_partitions"])
+            )
+            cls.max_partitions = max(
+                cls.nodes[key]["class_partition"], cls.max_partitions
+            )
 
-        return list(classes.values())
+            del cls.nodes[key]["methods"]
+            del cls.nodes[key]["method_partitions"]
 
     @classmethod
     def _method_link_to_class_link(cls, method_links: List) -> List:
         """ """
-        links = dict()
+
+        cls.links = defaultdict()
+
         for link in method_links:
-            source_class = ".".join(link["source"].split(".")[:-1])
-            target_class = ".".join(link["target"].split(".")[:-1])
+            # Class$1 represents an anonymous inner class, we don't wanna have
+            # these under consideration
+            source_class = ".".join(link["source"].split(".")[:-1]).split("$")[0]
+            # Class$1 represents an anonymous inner class, we don't wanna have
+            # these under consideration
+            target_class = ".".join(link["target"].split(".")[:-1]).split("$")[0]
+
+            if source_class not in cls.nodes or target_class not in cls.nodes:
+                continue
+
             link_type = link["type"]
             link_weight = link["weight"]
             if source_class != target_class:
-                if (source_class, target_class, link_type) not in links:
-                    links[(source_class, target_class, link_type)] = {
+                if (source_class, target_class, link_type) not in cls.links:
+                    cls.links[(source_class, target_class, link_type)] = {
                         "source": source_class,
                         "target": target_class,
                         "weight": link["weight"],
                         "type": link_type,
+                        "children": [
+                            {
+                                "source": link["source"],
+                                "target": link["target"],
+                            }
+                        ],
                     }
+
                 else:
-                    link_weight = links[(source_class, target_class, link_type)][
+                    link_weight = cls.links[(source_class, target_class, link_type)][
                         "weight"
                     ]
                     link_weight += 1
-                    links[(source_class, target_class, link_type)][
+                    cls.links[(source_class, target_class, link_type)][
                         "weight"
                     ] = link_weight
-
-        return list(links.values())
+                    cls.links[(source_class, target_class, link_type)][
+                        "children"
+                    ].append(
+                        {
+                            "source": link["source"],
+                            "target": link["target"],
+                        }
+                    )
 
     @classmethod
-    def from_method_graph_to_class_graph(cls, method_sdg_as_dict: Dict) -> Dict:
+    def _validate_nodes(cls):
+        for link in cls.links.values():
+            assert (
+                link["source"] in cls.nodes.keys()
+                and link["target"] in cls.nodes.keys()
+            )
+
+    @classmethod
+    def from_method_graph_to_class_graph(
+        cls, method_sdg_as_dict: Dict, transactions_as_dict: Dict = None
+    ) -> Dict:
         """
         Translate a method level SDG to a class level SDG.
 
@@ -95,7 +293,15 @@ class TransformGraph:
         method_nodes = method_sdg_as_dict["nodes"]
         method_links = method_sdg_as_dict["links"]
 
-        class_graph["nodes"] = TransformGraph._method_node_to_class_node(method_nodes)
-        class_graph["links"] = TransformGraph._method_link_to_class_link(method_links)
+        cls._method_node_to_class_node(method_nodes)
+        cls._method_link_to_class_link(method_links)
+
+        if transactions_as_dict is not None:
+            cls._transactions_to_graph(transactions_as_dict)
+
+        class_graph["nodes"] = list(cls.nodes.values())
+        class_graph["links"] = list(cls.links.values())
+
+        cls._validate_nodes()
 
         return class_graph
